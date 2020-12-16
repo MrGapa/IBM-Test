564925d77dd8b8462280482fa5f0ce140cb6d7da
Mrwindwaker <lagr45@gmail.com>
2020-12-15 23:02:39 -0600

    Stuff

--- app.js
+++ app.js
@@ -15,18 +15,12 @@
 **/
 
 var express = require('express');
-const message = require('./utils');
-var PORT;
-if (process.env.PORT) {
-  PORT = process.env.PORT;
-} else {
-  PORT = 80;
-}
+const PORT = process.env.PORT || 3000
+
 
 var app = express();
 app.get('/', function (req, res) {
-  res.send(message.getWelcomeMessage());
+  res.send('Hello World');
 });
 
-app.listen(PORT);
-console.log(message.getPortMessage() + PORT);
+app.listen(PORT, () => console.log('Server up on PORT:', PORT));
